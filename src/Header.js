17221f8879d850304280db10d0750e6703be64d0
Khorally <khorally.pierre@gmail.com>
2022-11-24 21:42:41 -0500

    header.js updates

--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,46 @@
 import React from "react";
-import "./Header.css"
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StoreIcon from '@mui/icons-material/Store';
-import SearchIcon from '@mui/icons-material/Search';
+import "./Header.css";
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SearchIcon from '@material-ui/icons/Search';
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
-function Header(){
+function Header() {
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className="header">
-<div className="App">
-      <div className='header_logo'> 
-        <StoreIcon className="header__logoImage" fontSize="large" />
-        <h2 className="header__logoTitle"> eShop</h2>
+          <Link to="/" style={{ textDecoration:"none" }}>
+            <div className="header__logo">
+                <StorefrontIcon className="header__logoImage" fontSize="large"/>
+                <h2 className="header__logoTitle">eShop</h2>
+            </div>
+          </Link>
+            
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
       </div>
-     
-      <div className="header_search"> 
-        <input type="text" className="header_searchInput" />
-        <SearchIcon className="header__searchIcon" fontSize="large" />
+      
+      <div className="header__nav">
+        <Link to="/login" style={{ textDecoration:"none" }}>
+          <div className="nav__item">
+            <span className="nav__itemLineOne">Hello Guest</span>
+            <span className="nav__itemLineTwo">Sign In</span>
+          </div>
+        </Link>
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <div className="nav__itemBasket">
+            <ShoppingBasketIcon/>
+            <span className="nav__itemLineTwo nav__basketCount">{basket.length}</span>
+          </div>
+        </Link>
       </div>
-      <div className="header_nav">
-        <div className="nav_item">
-          <spam className="nav_itemLineOne"> Hello Guest</spam>
-          <spam className="nav_itemLineTwo"> Sign In </spam>
-          
-        </div>
-        <div className="nav_item">
-          <spam className="nav_itemLineOne"> your </spam>
-          <spam className="nav_itemLineOne"> shop</spam>
-        </div>
-        <div className="nav_item">
-         <ShoppingBasketIcon fontSize="large" />
-         <h2 className="header"> eShop</h2>
-          <spam className="nav_itemLineOne"> 0</spam>
-        </div>
-      </div>
-  
-    </div>
         </div>
     )
 }
